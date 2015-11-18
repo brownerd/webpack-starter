15662ddc565fbabff86dd2b371765d718b918bfa
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-11-18 15:35:01 -0600

    first running version of webpack config with babel stylus and postcss

---
+++ README.md
@@ -0,0 +1,16 @@
+# Webpack Starter
+
+This is based off of:
+- [Black Triangle](https://github.com/jamesknelson/webpack-black-triangle) - [Unicorn Standard](http://unicornstandard.com/packages/boilerplate.html) and a few EggHead video tutorials. Still trying to grok Webpack setup.
+- [Webpack Made Simple: Building ES6 & LESS with autorefresh](http://jamesknelson.com/webpack-made-simple-build-es6-less-with-autorefresh-in-26-lines/)
+
+## Stack
+- Babel
+- Stylus
+- PostCSS
+    - Autoprefixer
+    - Pixrem
+    - PseudoElements
+
+## Issues
+I'm still new to this so I probably have some weird settings in here. But I have a point of reference now.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var autoprefixer = require('autoprefixer');
+var pixrem = require('pixrem');
+var pseudoelements = require('postcss-pseudoelements')
+
 module.exports = {
   entry: [
     'babel-polyfill',
@@ -36,16 +40,43 @@ module.exports = {
       {
         test: /\.styl$/,
         include: path.join(__dirname, 'src'),
-        loader: 'style-loader!css-loader!stylus-loader',
+        loader: 'style-loader!css-loader!postcss-loader!stylus-loader',
         exclude: /node_modules/
       },
+      // Load images
       {
-        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woof$|\.ttf$|\.otf$|\.eot$/,
-        loader: 'url-loader',
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/
+        test: /\.jpg/,
+        loader: "url-loader?limit=10000&mimetype=image/jpg"
+      },
+      {
+        test: /\.gif/,
+        loader: "url-loader?limit=10000&mimetype=image/gif"
+      },
+      {
+        test: /\.png/, loader: "url-loader?limit=10000&mimetype=image/png"
+      },
+      {
+        test: /\.svg/,
+        loader: "url-loader?limit=10000&mimetype=image/svg"
+      },
+
+      // Load fonts
+      {
+        test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [
+      autoprefixer({ browsers: ['ie >= 8', 'ie_mob >= 10', 'ff >= 3.6', 'chrome >= 10', 'safari >= 5.1', 'opera >= 11', 'ios >= 7', 'android >= 4.1', 'bb >= 10'] }),
+      pixrem,
+      pseudoelements
+    ];
   },
   debug: true
 };
